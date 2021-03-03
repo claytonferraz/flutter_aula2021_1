6627d8948849ed61f87a2285f89e3db9367b975e
clayton <clayton.andrade@ifro.edu.br>
2021-03-03 17:42:47 -0400

    Aula 1.2 Estados Material Theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,12 @@ void main() {
 
 class AppWidget extends StatelessWidget {
   final String title; //
-
   const AppWidget({Key? key, required this.title}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 100.0),
-        ),
-      ),
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green),
+      home: Container(child: Center(child: Text('Aula Material'))),
     );
   }
 }
