11eac83d26f27d047bee0955c82652e793f5e158
clayton <clayton.andrade@ifro.edu.br>
2021-03-03 17:32:16 -0400

    Aula 1.1 Estados - StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Aula Flutter',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 100.0),
+  runApp(AppWidget(title: 'Flutter'));
+}
+
+class AppWidget extends StatelessWidget {
+  final String? title; //
+
+  const AppWidget({Key? key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          'Aula Flutter com Classe',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 100.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
