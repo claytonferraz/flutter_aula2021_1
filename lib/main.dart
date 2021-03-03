fb4dc1e6c32a70803046da04ebdace5c60414b8e
clayton <clayton.andrade@ifro.edu.br>
2021-03-03 16:36:15 -0400

    aula 1.1

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Center(
+      child: Text(
+        'Flutterando',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 100.0),
+      ),
+    ),
+  ));
+}
