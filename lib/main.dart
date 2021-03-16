6f6018a52798c4094a74b4a7d7e5505ad0164f23
clayton <clayton.andrade@ifro.edu.br>
2021-03-15 21:52:40 -0400

    Aula 1.3
    setState() e statfull

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,32 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.green),
-      home: Container(child: Center(child: Text('Aula Material'))),
+      home: HomePage(),
     );
+  }
+}
+
+// sempre se cria uma classe que vai dentro da classe StateFul
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+            child: GestureDetector(
+                child: Text('Click: $counter'),
+                onTap: () {
+                  // ao colocar o setState vai mudar o estado
+                  setState(() {
+                    counter++;
+                  });
+                })));
   }
 }
