23bbc3b36de2cd000323877a7fc7327b82af284b
clayton <clayton.andrade@ifro.edu.br>
2021-03-15 23:24:09 -0400

    Aula 1.7.4 AppController e AppWidget
    mudar o estado das cores

---
+++ lib/app_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// muda o estado dos dados, e não tela
+class AppController extends ChangeNotifier {
+  // design pattern singleton
+  static AppController instance = new AppController();
+
+  bool isDarkTheme = false;
+  changeTheme() {
+    // contrário de um boolean
+    isDarkTheme = !isDarkTheme;
+    notifyListeners();
+  }
+}
--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import a nova class gerada
+import 'app_controller.dart';
 import 'home_page.dart';
 
 // fazer os imports
@@ -9,12 +10,20 @@ class AppWidget extends StatelessWidget {
   // retiramos o atributo title
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        brightness: Brightness.dark,
-      ),
-      home: HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (BuildContext context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            brightness: Brightness.dark,
+          ),
+          home: HomePage(),
+        );
+      },
     );
   }
 }
+
+// primarySwatch: Colors.green,
+// brightness: Brightness.dark,
