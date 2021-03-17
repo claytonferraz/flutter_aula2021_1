01be89367ba7768ecf79fe19db3df6d87100a91b
clayton <clayton.andrade@ifro.edu.br>
2021-03-17 17:57:25 -0400

    Aula 1.11.3
    Direção list view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,6 +27,8 @@ class HomePageState extends State<HomePage> {
         width: double.infinity,
         height: double.infinity,
         child: ListView(
+          // scrollDirection: Axis.horizontal,
+          scrollDirection: Axis.vertical,
           children: [
             Text('Contador: $counter'),
             Container(
