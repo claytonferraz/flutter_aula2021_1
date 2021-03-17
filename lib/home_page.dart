001e55f2d375fc8d8787dbd5e7348216c7f89756
clayton <clayton.andrade@ifro.edu.br>
2021-03-17 17:29:46 -0400

    Aula 1.9.2
    Colunas com tamanho

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,6 +26,8 @@ class HomePageState extends State<HomePage> {
         width: double.infinity,
         height: double.infinity,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Contador: $counter'),
             CustomSwitch(),
