f2df02f682b4deddbcc6be949e382d95dc70f245
clayton <clayton.andrade@ifro.edu.br>
2021-03-17 20:06:53 -0400

    Aula 1.12.4
    Tela login com Scaffold

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,45 +10,47 @@ class _LoginPageState extends State<LoginPage> {
   String senha = "";
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                onChanged: (text) => email = text,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) => email = text,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                onChanged: (text) => senha = text,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (email == 'clayton@gmail.com' && senha == '123') {
-                      print('passou');
-                    }
-                  },
-                  child: Text('Login')),
-            ],
+                TextField(
+                  onChanged: (text) => senha = text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (email == 'clayton@gmail.com' && senha == '123') {
+                        print('passou');
+                      }
+                    },
+                    child: Text('Login')),
+              ],
+            ),
           ),
         ),
       ),
