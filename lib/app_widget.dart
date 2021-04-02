f31cd4d391b7e0f20797e79a255a83f3653a4fa8
clayton <clayton.andrade@ifro.edu.br>
2021-04-01 21:11:52 -0400

    Aula 1.17
    Stack:  plano de fundo

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // import a nova class gerada
 import 'app_controller.dart';
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,62 +8,83 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 90,
-                  height: 50,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(
-                  height: 40,
-                ),
-                TextField(
-                  onChanged: (text) => email = text,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email User',
-                    border: OutlineInputBorder(),
+
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 90,
+                    height: 50,
+                    child: Image.asset('assets/images/logo.png'),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) => senha = text,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
+                  Container(
+                    height: 40,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (email == 'clayton@gmail.com' && senha == '123') {
-                        print('passou');
-                        Navigator.of(context).pushNamed('/home');
-                      } else {
-                        print('Login Errado');
-                      }
-                    },
-                    child: Text('Login')),
-              ],
+                  TextField(
+                    onChanged: (text) => email = text,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email User',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    onChanged: (text) => senha = text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (email == 'clayton@gmail.com' && senha == '123') {
+                          print('passou');
+                          Navigator.of(context).pushNamed('/home');
+                        } else {
+                          print('Login Errado');
+                        }
+                      },
+                      child: Text('Login')),
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // aqui ele empilha os componentes
+          SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset(
+                'assets/images/Night.png',
+                fit: BoxFit.cover,
+              )),
+          Container(color: Colors.black.withOpacity(0.05)),
+          _body(),
+        ],
       ),
     );
   }
